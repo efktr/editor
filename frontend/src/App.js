2a1c57b2a556e01bd371e9b1de9df80eaf834a96
Christian Dallago <code@dallago.us>
2017-05-05 18:31:47 +0200

    removed unnecessary libreries and added fields to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import EfktrBody from 'efktr-body/lib';
+import data from '../../body/data/T029Dictionary.json';
 
 class App extends Component {
 
@@ -10,13 +11,13 @@ class App extends Component {
         this.state = {
             latlng: undefined,
             side: undefined,
-            data: []
-        }
+            data: data
+        };
+
+        this.updateFields = this.updateFields.bind(this);
     }
 
     updateFields(data){
-        console.log('hello');
-        console.log(data);
         this.setState({
             latlng: data.latlng,
             side: data.side
@@ -29,25 +30,30 @@ class App extends Component {
                 <div className="App-header">
                     <h3>Body Editor</h3>
                 </div>
-                <EfktrBody back={true} onClick={this.updateFields}/>
+                <EfktrBody back={this.state.side} onClick={this.updateFields}/>
                 <div className="ui form">
                     <div className="fields">
                         <div className="field">
                             <select className="ui search dropdown">
                                 {this.state.data.map( e => {
-                                    return <option>{e.definition}</option>
+                                    return <option value={e.cui}>{e.definition}</option>
                                 })}
                             </select>
                         </div>
                     </div>
                     <div className="two fields">
                         <div className="field">
-                            <label>latlng</label>
-                            <input type="text" disabled={true} value={this.state.latlng}/>
+                            <label>Position</label>
+                            <input type="text" disabled={true} value={JSON.stringify(this.state.latlng)}/>
                         </div>
                         <div className="field">
                             <label>side</label>
                             <input type="text" disabled={true} value={this.state.side}/>
+                        </div>
+                    </div>
+                    <div className="fields">
+                        <div className="field">
+                            <input className="ui button positive attached" type="button" value="Submit"/>
                         </div>
                     </div>
                 </div>
